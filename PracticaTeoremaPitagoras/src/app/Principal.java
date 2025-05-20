60b175299876e02a3f9aae8d6998f71367f9d5f9
ndmario96 <ndiazmario@gmail.com>
2025-05-20 16:25:23 +0200

    Clase principal creada y depuracion

---
+++ PracticaTeoremaPitagoras/src/app/Principal.java
@@ -0,0 +1,21 @@
+package app;
+
+import clases.TrianguloRectangulo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		TrianguloRectangulo t1 = new TrianguloRectangulo(2,3);
+		
+		TrianguloRectangulo t2 = new TrianguloRectangulo();
+		
+		t1.area();
+		t1.perimetro();
+		
+		t2.area();
+		t2.perimetro();
+		
+	}
+
+}
--- PracticaTeoremaPitagoras/src/clases/TrianguloRectangulo.java
+++ PracticaTeoremaPitagoras/src/clases/TrianguloRectangulo.java
@@ -1,16 +1,38 @@
 package clases;
-
+/**
+ * Esta es la clase Triangulo Rectangulo
+ * 
+ */
 public class TrianguloRectangulo {
+/**
+ * La variable a representa un cateto.
+ * 
+ * La variable b representa otro cateto
+ * 
+ * La variable c es la hipotenusa
+ * 
+ */
  double a;
  double b; 
  double c;
  
+ /**
+  * Este constructor crea por defecto un triángulo con los catetos en valor 1 y 1. 
+  * 
+  */
  public TrianguloRectangulo() {
 	 this.a = 1;
 	 this.b = 1;
 	 this.c = hipotenusa();
  }
  
+ /**
+  * Este es el constructor para crear un triangulo rectangulo con los valores que se le pasan por parametro.
+  * Si los catetos son menores o iguales a 0, lanza una excepcion.
+  * 
+  * @param a Es el cateto 1, tipo double
+  * @param b Es el cateto 2, tipo double 
+  */
  public TrianguloRectangulo (double a, double b) {
 	 if (a <= 0 || b <= 0) {
 		 throw new IllegalArgumentException ("El numero debe ser mayor a 0");
@@ -22,16 +44,31 @@ public class TrianguloRectangulo {
 	 }
  }
  
+ /**
+  * Metodo para calcular el area del triangulo 
+  * 
+  * @return devuelve el area tipo double 
+  */
  public double area() {
 	 return ((a*b)/2.0);
  }
  
+ /**
+  * Metodo para calcular la hipotenusa del triangulo
+  * 
+  * @return devuelve la hipotenusa tipo double
+  */
  public double hipotenusa() {
 	 double interiorRaiz = ((a*a) + (b*b));
 	 double hipotenusa = Math.sqrt(interiorRaiz);
 	 return hipotenusa;
  }
  
+ /**
+  * Método para calcular el perimetro del triangulo
+  * 
+  * @return devuelve el perimetro tipo double 
+  */
  public double perimetro () {
 	 return a + b + c;
  }
